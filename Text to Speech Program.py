c01634be562b4d24483bf7655c07b0f394169437
UjvalMadhu <93603056+UjvalMadhu@users.noreply.github.com>
2022-08-01 23:28:10 +0530

    Update Text to Speech Program.py

--- Text to Speech Program.py
+++ Text to Speech Program.py
@@ -6,6 +6,13 @@ import tkinter
 import playsound
 import os
 from gtts import gTTS
+# We create a function that can say a personalised welcome message with an input text
+def TTS(text1):
+    texttoSpeech = gTTS("Welcome " + text1)                  # gTTS function converts the text into speech
+    texttoSpeech.save("Speech1.mp3")                         # We save the file as an mp3 file
+    playsound.playsound('type in the address to which Speech1 is stored', True)  # Specify the address and use the playsound function to play the saved file
+    os.remove('Speech1.mp3')                                 # The os.remove function is used to remove the file path
+
 
 mywindow= tkinter.Tk()               # The tkinter library is used to create a window where we can display a message.
 mywindow.title("Greeting Window")
@@ -15,13 +22,9 @@ tkinter.Label(mywindow,text="Greetings Window ",font =("Arial",16)).place(x=170,
 tkinter.Label(mywindow,text="Please Enter Your Name: ",font =("Arial",16)).place(x=0,y=100)
 name = tkinter.Entry(mywindow,font =("Arial",16),width=30)
 name.place(x=250,y=100)
-def TTS(text1):
-    texttoSpeech = gTTS("Welcome " + text1)
-    texttoSpeech.save("Speech1.mp3")
-    playsound.playsound('C:/Users/asus/Desktop/Srishti Robotics/2. Python/Speech1.mp3', True)
-    os.remove('Speech1.mp3')
-    
 
+    
+# Here we create a function that will be called for the enter button we are about t create.
 def submit():
     text1 = name.get()
     TTS(text1)
@@ -29,4 +32,4 @@ def submit():
 mybutton = tkinter.Button(mywindow,text="Enter",font =("Arial",16), command = submit).place(x=200,y=150)
 
 
-mywindow.mainloop() 
+mywindow.mainloop()     # This function tells python to keep the tkinter window running until it is closed.
